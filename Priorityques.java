970a987b2a7ef25b6b1ac0683916a4b86748029c
Prathamesh-Pulkanthwar <Pulkanthwarprathamesh2@gmail.com>
2024-01-18 15:06:03 +0530

    Priorityques

---
+++ Priorityques.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Priorityques {
+    public static void main(String[] args) {
+
+        //Priority Queue = A FIFO data structure that serves elements
+        //                 with the highest priorities first
+        //                 before elements with lower priority
+
+        Queue<Double> queue =new PriorityQueue<>();
+
+        queue.offer(2.4);
+        queue.offer(1.4);
+        queue.offer(5.4);
+        queue.offer(28.4);
+
+        System.out.println(queue);
+    }
+}
